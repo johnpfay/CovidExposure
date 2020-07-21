b9d8e69a52f2c02147ed3ce53dc1be6df1530117
John Fay <john.fay@duke.edu>
2020-07-21 15:12:41 -0400

    fixed quantile computation (thanks Prasad!)

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def summarize_output(df,faculty=True):
     else: fld = 'PS_Ssemester'; txt = 'Student'
     #Create markdown from values
     the_mean = df[fld].mean()
-    the_quants = [df[fld].quantile(x) for x in (0.05,0.25,0.5,0.75,0.9)]
+    the_quants = [df[fld].quantile(x) for x in (0.05,0.25,0.5,0.75,0.95)]
     #Create Markdown
     md_text=f'''  
 **Average Infection Probability for {txt} for semester**
